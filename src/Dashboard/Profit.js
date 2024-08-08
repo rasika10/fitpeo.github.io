2b367f18caeb4f7ea1dfbeff69568373edd346a4
rasika10 <43296882+rasika10@users.noreply.github.com>
2024-08-08 12:31:37 +0530

    Update Profit.js

--- src/Dashboard/Profit.js
+++ src/Dashboard/Profit.js
@@ -1,12 +1,9 @@
 import * as React from "react";
-import Link from "@mui/material/Link";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Title from "./Title";
 import ArrowDropUpOutlinedIcon from "@mui/icons-material/ArrowDropUpOutlined";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import { secondaryListItems } from "./listItems";
 
 function preventDefault(event) {
   event.preventDefault();
